4fada54955419e061ea7402f4e3a7ba36dc6d23b
Youssef Samih <youssef.samih@4d.com>
2020-07-10 02:20:05 +0100

    finish isolated test examples

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,28 @@
+import { MessageService } from './message.service';
+
+describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+    service = new MessageService();
+  });
+
+  it('should have no messages to start', () => {
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('should add a message when add is called', () => {
+    service.add('message 1');
+
+    expect(service.messages.length).toBe(1);
+  });
+
+  it('should remove all messages when is called', () => {
+    service.add('message 1');
+
+    service.clear();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+});
