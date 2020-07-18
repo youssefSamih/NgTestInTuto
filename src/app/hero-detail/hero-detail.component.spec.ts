f8fb7d7339cd7314cb794abe68e58ae804cdd850
Youssef Samih <youssef.samih@4d.com>
2020-07-18 18:57:07 +0100

    finish unit test tuto

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, flush, async } from '@angular/core/testing';
 import { HeroDetailComponent } from './hero-detail.component';
 import { ActivatedRoute } from '@angular/router';
 import { HeroService } from './../hero.service';
@@ -34,9 +34,30 @@ describe('HeroDetailComponent', () => {
     mockHeroService.getHero.and.returnValue(of({ id: 3, name: 'SuperDude', strength: 180 }));
   });
 
-  it('', () => {
+  it('should render hero name in a h2 tag', () => {
     fixture.detectChanges();
 
     expect(fixture.nativeElement.querySelector('h2').textContent).toContain('SUPERDUDE');
   });
+
+  // it('should call updateHero when save is called', fakeAsync(() => {
+  //   mockHeroService.updateHero.and.returnValue(of({}));
+  //   fixture.detectChanges();
+
+  //   fixture.componentInstance.save();
+  //   flush();
+
+  //   expect(mockHeroService.updateHero).toHaveBeenCalled();
+  // }));
+
+  it('should call updateHero when save is called', async(() => {
+    mockHeroService.updateHero.and.returnValue(of({}));
+    fixture.detectChanges();
+
+    fixture.componentInstance.save();
+
+    fixture.whenStable().then(() => {
+      expect(mockHeroService.updateHero).toHaveBeenCalled();
+    });
+  }));
 });
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
+import { resolve } from 'path';
 
 @Component({
   selector: 'app-hero-detail',
@@ -33,8 +34,33 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
- save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+  save(): void {
+    let p = new Promise((resolve) => {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+      resolve();
+    });
   }
+
+//  save(): void {
+//    debounce(() => {
+//      this.heroService.updateHero(this.hero)
+//        .subscribe(() => this.goBack());
+//    }, 250, false)();
+//   }
+}
+
+function debounce(func, wait, immediate) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) { func.apply(context, args); }
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) { func.apply(context, args); }
+  };
 }
